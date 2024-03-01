47e2071f603ce594f3ff4028b75e8e1a0fe9adfa
macli <waleedzafar4578@gmail.com>
2024-03-01 10:53:15 +0500

    write basic triadic_logic which store status of variable or value

--- app/src/main.rs
+++ app/src/main.rs
@@ -1,7 +1,17 @@
+use triadic_logic::Degree;
 
 fn main() {
-    compiler::hello();
-    server::hello();
-    storge::hello();
-    triadic_logic::hello();
+    //#1
+    //compiler::hello();
+    //server::hello();
+    //storge::hello();
+    //triadic_logic::hello();
+    //#2
+    let mut value:Degree=Degree::default();
+    println!("Status of the value is: {}",value);
+    value=Degree::F;
+    println!("Status of the value is: {}",value);
+    value=Degree::T;
+    println!("Status of the value is: {}",value);
+
 }
--- triadic_logic/src/lib.rs
+++ triadic_logic/src/lib.rs
@@ -1,3 +1,32 @@
+use serde::{Deserialize, Serialize};
+use std::fmt;
+use std::fmt::Formatter;
+
+#[derive(Serialize,Deserialize,Debug)]
+pub enum Degree{
+    //Here T is represented status of value is true
+    //Here F is represented status of value is false
+    //Here L is represented status of value is limit
+    T,F,L,
+}
+#[allow(dead_code)]
+impl Degree{
+    pub fn default()->Self{
+        Degree::L
+    }
+}
+impl fmt::Display for Degree{
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        let display_str=match self {
+            Degree::T => {"T"}
+            Degree::F => {"F"}
+            Degree::L => {"L"}
+        };
+        write!(f,"{}",display_str)
+    }
+}
+
 pub fn hello(){
     println!("Hello from triadic_logic side");
 }
+
