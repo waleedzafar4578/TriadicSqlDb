d1db382c764369ed39f6a1dd6ddbbfa0a924ad55
macli <waleedzafar4578@gmail.com>
2024-03-06 23:44:17 +0500

    Adding basic elements in react app which show input bar to pass query and show result

--- web_app/react-app/src/App.jsx
+++ web_app/react-app/src/App.jsx
@@ -1,14 +1,46 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
+  const [inputMessage, setInputMessage] = useState('');
+  const [outputMessage, setOutputMessage] = useState('');
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch('http://localhost:8080/process_json', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          message: inputMessage }),
+      });
+
+      const data = await response.json();
+      setOutputMessage(data.reversed_message);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  };
+
+  const handleButtonClick = () => {
+    fetchData();
+  };
 
   return (
-    <h1>Hello from this</h1>
-  )
+      <div>
+        <h1>React App Sending and Receiving JSON</h1>
+        <label>
+          Input Message:
+          <input
+              type="text"
+              value={inputMessage}
+              onChange={(e) => setInputMessage(e.target.value)}
+          />
+        </label>
+        <button onClick={handleButtonClick}>Send to Server</button>
+        <p>Output Message: {outputMessage}</p>
+      </div>
+  );
 }
 
-export default App
+export default App;
